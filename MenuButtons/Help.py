d287c44f4560201493e0c7ba1891ee98dd5d04f1
Channing Norton <cnorton@pc-solutions.it>
2023-11-12 17:56:17 -0500

    Fixed Help.py. It now works... Better. I'd like to add some zones and responsiveness, but that's for later.

--- MenuButtons/Help.py
+++ MenuButtons/Help.py
@@ -20,12 +20,12 @@ class HelpDialog(ttk.Frame):
         self.frame.grid(column=0, row=0, sticky="N, W, E, S")
         self.frame.columnconfigure(0, weight=1)
         self.frame.rowconfigure(0, weight=1)
-        ttk.Label(
+        ttk.Label(self.frame,
             text="ArchiveSpace Bulk Edit UI can be found on GitHub for complete information on the program. To use, "
                  "first create a connection, then create a query. BACK UP YOUR DATA BEFORE RUNNING A QUERY, AND "
                  "ENSURE YOU WILL HAVE A STABLE SERVER CONNECTION. Failure to do so can cause issues with data "
                  "consistency in your instance.").grid(column=1, row=1)
-        ttk.Label(
+        ttk.Label(self.frame,
             text="Credit to Channing Norton in collaboration with the University of Rochester. Code licensed under "
                  "the Mozzila Public License version 2.0").grid(column=1, row=2)
         ttk.Button(self.frame, width=70, text="Close", command=self.closeWindow).grid(column=1, row=4,
