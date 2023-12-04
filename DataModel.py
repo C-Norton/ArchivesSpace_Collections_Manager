fdfa81bfbc8260865b006411726502c2d0351067
Channing Norton <cnorton@pc-solutions.it>
2023-12-03 21:32:48 -0500

    Started fleshing out data model

---
+++ DataModel.py
@@ -0,0 +1,31 @@
+"""
+Needed classes
+
+QueryType
+    Equality
+    Presence
+    BeginsWith
+    EndsWith
+Query
+Operation
+    not
+    or
+    and
+    submit
+FieldType
+    IntField
+    DateField
+    ETC
+Field
+RecordType
+    DigitalObject
+    Resource
+    Item
+    ETC
+Record
+Log (Use PANDAS)
+
+So what does the dataflow actually look like
+First build a query, A Query consists of QueryType, combined with operations
+Query needs to be a tree
+"""
