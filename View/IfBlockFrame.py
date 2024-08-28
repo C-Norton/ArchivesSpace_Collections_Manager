5febaed17919f27c9461392898b1480c91fc5c58
channing <cnorton9@ur.rochester.edu>
2024-08-28 17:48:22 -0400

    Added buttons to IfBlockFrame.py
    Authored QueryManager.py

--- View/IfBlockFrame.py
+++ View/IfBlockFrame.py
@@ -9,23 +9,32 @@ import Model.QueryType
 
 # this will need to be a grid type
 class IfBlockFrame(ttk.Frame):
+
+
     def __init__(self, parent: MasterFrame):
         super().__init__(master=parent, padding="3 3 12 12")
         self.masterframe = parent
         self.pack(side="bottom", fill="x")
         self.width = 1
         self.height = 1
-        self.drawIfBlock()
+        self.draw_if_block()
 
-    def drawIfBlock(self):
+    def draw_if_block(self):
         ttk.Label(self, text="If").grid(row=0, column=0)
-        field = tkinter.StringVar()
+        self.field = tkinter.StringVar()
         ttk.OptionMenu(
-            self, field, "id_0", *[e.name for e in Model.ResourceField.ResourceField]
+            self, self.field, "id_0", *[e.name for e in Model.ResourceField.ResourceField]
         ).grid(row=0, column=1)
-        querytype = tkinter.StringVar()
+        self.querytype = tkinter.StringVar()
         ttk.OptionMenu(
-            self, querytype, "EQUALS", *[e.name for e in Model.QueryType.QueryType]
+            self, self.querytype, "EQUALS", *[e.name for e in Model.QueryType.QueryType]
         ).grid(row=0, column=2)
-        input = tkinter.StringVar()
-        ttk.Entry(self, textvariable=input, width=35).grid(row=0, column=3)
+        self.input = tkinter.StringVar()
+        ttk.Entry(self, textvariable=self.input, width=35).grid(row=0, column=3)
+        ttk.Button(self,text="Add").grid(row=0, column=4, command = self.query_add)
+        ttk.Button(self,text="Submit").grid(row=1, column=0, columnspan=5, command=self.submit_query)
+
+    def submit_query(self):
+        pass
+    def query_add(self):
+        pass
