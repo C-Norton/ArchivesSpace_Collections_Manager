2600eef7e00e215d7f3b91d48980c74e4077105c
Channing Norton <24469058+C-Norton@users.noreply.github.com>
2024-09-04 07:39:33 -0400

    Created resource, reformated a few files with black

---
+++ Model/Resource.py
@@ -0,0 +1,66 @@
+"""
+The goal of the resource class is to unpack the JSON data we get from archivessnake into a more usable format, allow
+it to be updated, then validate and repack it before submitting it back to archivessnake
+"""
+
+import json
+
+
+class Resource:
+    def __init__(self):
+        self.data = dict()
+        self.data["id_0"] = ""
+        self.data["id_1"] = ""
+        self.data["id_2"] = ""
+        self.data["id_3"] = ""
+        self.data["external_ark_url"] = ""
+        self.data["import_current_ark"] = ""
+        self.data["import_previous_arks"] = []
+        self.data["level"] = ""
+        self.data["other_level"] = ""
+        self.data["slug"] = ""
+        self.data["is_slug_auto"] = False
+        self.data["resource_type"] = ""
+        self.data["tree"] = None
+        self.data["restrictions"] = False
+        self.data["repository_processing_note"] = ""
+        self.data["ead_id"] = ""
+        self.data["ead_location"] = ""
+        self.data["finding_aid_title"] = ""
+        self.data["finding_aid_subtitle"] = ""
+        self.data["finding_aid_filing_title"] = ""
+        self.data["finding_aid_date"] = ""
+        self.data["finding_aid_author"] = ""
+        self.data["finding_aid_description_rules"] = ""
+        self.data["finding_aid_language"] = ""
+        self.data["finding_aid_script"] = ""
+        self.data["finding_aid_language_note"] = ""
+        self.data["finding_aid_sponsor"] = ""
+        self.data["finding_aid_edition_statement"] = ""
+        self.data["finding_aid_series_statement"] = ""
+        self.data["finding_aid_status"] = ""
+        self.data["finding_aid_note"] = ""
+        self.data["lang_materials"] = []
+        self.data["extents"] = []
+        self.data["revision_statements"] = []
+        self.data["dates"] = []
+        self.data["instances"] = []
+        self.data["deaccessions"] = []
+        self.data["collection_management"] = json
+        self.data["user_defined"] = json
+        self.data["related_accessions"] = []
+        self.data["classifications"] = []
+        self.data["notes"] = []
+        self.data["ark_name"] = json
+        self.data["metadata_rights_declarations"] = []
+
+    def __init__(self, data):
+        pass
+
+    @classmethod
+    def from_dict(cls, data):
+        data = data
+
+    @classmethod
+    def from_json(cls, data):
+        pass
--- Model/SubNote.py
+++ Model/SubNote.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class SubNote:
     Content: str
