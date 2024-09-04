eb416cfc7a740dcfb337ed12a4ddbb8aff6aa476
Channing Norton <24469058+C-Norton@users.noreply.github.com>
2024-09-03 23:54:20 -0400

    Worked more towards creating a note construction system in the view

--- View/NoteConstructionModalPopup.py
+++ View/NoteConstructionModalPopup.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, Toplevel, StringVar
+from tkinter import ttk, Toplevel, StringVar, BooleanVar
 from tkinter import Text
 import Model.NoteType
 from View import MasterFrame
@@ -15,6 +15,10 @@ class NoteConstructionModalPopup:
 
         self.note_type = StringVar()
         self.note_value = StringVar()
+        self.publish = BooleanVar()
+        self.publish.set(False)
+        self.label = StringVar()
+        self.persistent_id = StringVar()
         self.draw_note_definition_menu()
         self.frame.focus_set()
         self.frame.grab_set()
@@ -23,8 +27,22 @@ class NoteConstructionModalPopup:
         ttk.Label(self.frame, text="Define Note").grid(row=0, column=0)
         ttk.OptionMenu(
             self.frame, self.note_type, *[e.name for e in Model.NoteType.NoteType]
-        ).grid(row=1, column=0)
-        Text(self.frame).grid(row=2, column=0)
+        ).grid(row=1, column=0, columnspan=2)
+        ttk.Label(self.frame, text="Note Content (required)").grid(
+            row=2, column=0, columnspan=2
+        )
+        Text(self.frame).grid(row=3, column=0, columnspan=2)
+        ttk.Checkbutton(self.frame, text="Publish?", variable=self.publish).grid(
+            row=4, column=0, columnspan=2
+        )
+        self.publish.set(self.publish.get())
+        ttk.Label(self.frame, text="Label").grid(row=5, column=0)
+        ttk.Entry(self.frame, textvariable=self.label).grid(row=5, column=1)
+        ttk.Label(self.frame, text="Persistent ID").grid(row=6, column=0)
+        ttk.Entry(self.frame, textvariable=self.persistent_id).grid(row=6, column=1)
 
     def layout_manager(self):
         pass
+
+    def validate(self) -> bool:
+        raise NotImplementedError
