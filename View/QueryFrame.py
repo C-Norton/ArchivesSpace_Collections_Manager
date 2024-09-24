7e427c435e092d5c70c0697040756a0879acacf3
Channing Norton <cnorton@pc-solutions.it>
2024-09-24 09:37:06 -0400

    added drawqueryframe to hopefully be layout for individual queries. Untested

--- View/QueryFrame.py
+++ View/QueryFrame.py
@@ -9,11 +9,22 @@ class QueryFrame(ttk.Frame):
 
     def __init__(self, parent: MasterFrame):
         super().__init__(master=parent, padding="3 3 12 12")
-        self.masterframe = parent
+        self.master_frame = parent
+        self.frame = ttk.Frame(master=self.master_frame)
         self.pack(side="bottom", fill="x")
         self.width = 1
         self.height = 1
         self.draw_query_frame(None)
 
-    def draw_query_frame(self, query: str):
+    def draw_query_frame(self, query: str, depth :int):
+        for i in range(depth):
+            label = ttk.Label(self, text="    ")
+            label.pack(side="left", fill="x")
+        label = ttk.Label(self, text=query)
+        label.pack(side="left", fill="x")
+        operator = ttk.OptionMenu(self.frame, self.master_frame.operators, *["AND", "OR"])
+        operator.pack(side="left", fill="x")
+        remove = ttk.Button(self.frame,name="Remove",command=self.remove)
+        remove.pack(side="left", fill="x")
+    def remove(self):
         pass
